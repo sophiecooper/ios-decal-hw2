5e5d45581736a8bcff707c778be486b3c79bdea1
Sophie Cooper <sophie@cooperzone.net>
2016-02-15 22:57:54 -0800

    keyboard finished

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -9,10 +9,87 @@
 import UIKit
 
 class KeyboardViewController: UIInputViewController {
-
-    @IBOutlet var nextKeyboardButton: UIButton!
     
     var keyboardView: UIView!
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+    @IBOutlet var returnButton: UIButton!
+    @IBOutlet var deleteButton: UIButton!
+    @IBOutlet var milk: UIButton!
+    @IBOutlet var eggs: UIButton!
+    @IBOutlet var bread: UIButton!
+    @IBOutlet var orange: UIButton!
+    @IBOutlet var cheese: UIButton!
+    @IBOutlet var cookie: UIButton!
+    @IBOutlet var apple: UIButton!
+    @IBOutlet var banana: UIButton!
+    @IBOutlet var lettuce: UIButton!
+    @IBOutlet var grapes: UIButton!
+    @IBOutlet var juice: UIButton!
+    @IBOutlet var carrot: UIButton!
+    
+    
+    
+    func returnPressed(){
+        textDocumentProxy.insertText("\r\n")
+    }
+    
+    func deletePressed(){
+        textDocumentProxy.deleteBackward()
+    }
+    
+    func milkPressed(){
+        textDocumentProxy.insertText("milk ")
+    }
+    
+    func eggsPressed(){
+        textDocumentProxy.insertText("eggs ")
+    }
+    
+    func breadPressed(){
+        textDocumentProxy.insertText("bread ")
+    }
+    
+    func orangePressed(){
+        textDocumentProxy.insertText("orange ")
+    }
+    
+    func cheesePressed(){
+        textDocumentProxy.insertText("cheese ")
+    }
+    
+    func cookiePressed(){
+        textDocumentProxy.insertText("cookie ")
+    }
+    
+    func applePressed(){
+        textDocumentProxy.insertText("apple ")
+    }
+    
+    func bananaPressed(){
+        textDocumentProxy.insertText("banana ")
+    }
+    
+    func lettucePressed(){
+        textDocumentProxy.insertText("lettuce ")
+    }
+    
+    func grapesPressed(){
+        textDocumentProxy.insertText("grapes ")
+    }
+    
+    func juicePressed(){
+        textDocumentProxy.insertText("juice ")
+    }
+    
+    func carrotPressed(){
+        textDocumentProxy.insertText("carrot ")
+    }
+    
+    
+    
+    
+    
 
     override func updateViewConstraints() {
         super.updateViewConstraints()
@@ -44,7 +121,22 @@ class KeyboardViewController: UIInputViewController {
         keyboardView.frame = view.frame
         view.addSubview(keyboardView)
         view.backgroundColor = keyboardView.backgroundColor
-        nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
+        nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
+        returnButton.addTarget(self, action: "returnPressed", forControlEvents: .TouchUpInside)
+        deleteButton.addTarget(self, action: "deletePressed", forControlEvents: .TouchUpInside)
+        milk.addTarget(self, action: "milkPressed", forControlEvents: .TouchUpInside)
+        eggs.addTarget(self, action: "eggsPressed", forControlEvents: .TouchUpInside)
+        bread.addTarget(self, action: "breadPressed", forControlEvents: .TouchUpInside)
+        orange.addTarget(self, action: "orangePressed", forControlEvents: .TouchUpInside)
+        cheese.addTarget(self, action: "cheesePressed", forControlEvents: .TouchUpInside)
+        carrot.addTarget(self, action: "carrotPressed", forControlEvents: .TouchUpInside)
+        cookie.addTarget(self, action: "cookiePressed", forControlEvents: .TouchUpInside)
+        apple.addTarget(self, action: "applePressed", forControlEvents: .TouchUpInside)
+        banana.addTarget(self, action: "bananaPressed", forControlEvents: .TouchUpInside)
+        lettuce.addTarget(self, action: "lettucePressed", forControlEvents: .TouchUpInside)
+        grapes.addTarget(self, action: "grapesPressed", forControlEvents: .TouchUpInside)
+        juice.addTarget(self, action: "juicePressed", forControlEvents: .TouchUpInside)
+        
         
     }
 
